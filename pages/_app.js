63b4e39f6e5416aaf67a5b4cfcd3491b89460ede
Munozovepi Gwata <muno.gwata@gmail.com>
2022-04-30 23:10:28 +0200

    added the required color and font styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import Layout from '../components/universal/Layout'
 
 function MyApp({ Component, pageProps }) {
@@ -7,10 +7,6 @@ function MyApp({ Component, pageProps }) {
   <Component {...pageProps} />
   </Layout>
   )
-  
-  
-  
-
 
 }
 
