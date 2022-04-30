4a148aae18d34fd04b1e54c1ed7fdcf2b6f9d7b1
Munozovepi Gwata <muno.gwata@gmail.com>
2022-04-30 23:48:04 +0200

    adding footer navigation

--- components/universal/Footer.js
+++ components/universal/Footer.js
@@ -1,10 +1,33 @@
-
+import footerstyles from '../../styles/components/Footer.module.scss';
+import Link from 'next/link';
 
 
 const Footer = () => { 
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className={footerstyles.footerContainer}>
+
+            <div className={footerstyles.footerContentContainer}>
+            <div className={footerstyles.footerColumnOne}>
+                <span className={footerstyles.textLogo}>
+                    <Link href='/'>
+                    <a>
+                    astute
+                    </a>
+                    </Link>
+                    </span>
+                    <span className={footerstyles.textLogo}>
+                    <Link href='/about'>
+                    <a>
+                 About
+                    </a>
+                    </Link>
+                    </span>
+              
+            </div>
+            
+            
+            </div>
+            
         </div>
     )
 }
---
+++ pages/about.js
@@ -0,0 +1,15 @@
+
+
+export default function About() {
+    return (
+      <div >
+  
+  
+        
+          <h1>
+           About Us - we are amazing period
+          </h1>
+  
+      </div>
+    )
+  }
