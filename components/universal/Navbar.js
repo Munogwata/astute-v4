2670bf73518c11a684be0a8aee39e236afde80b4
Munozovepi Gwata <muno.gwata@gmail.com>
2022-05-09 20:15:25 +0200

    added the skeleton of the navigation bar just need to style

--- components/universal/Navbar.js
+++ components/universal/Navbar.js
@@ -1,104 +1,33 @@
 import navbarstyles from "../../styles/components/Navbar.module.scss";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { useState } from "react";
 
 function Navbar() {
-  const router = useRouter();
-  const [makeHover, viewMakeSublinks] = useState(false);
-  const [keepHover, viewKeepSublinks] = useState(false);
-  const [growHover, viewGrowSublinks] = useState(false);
-  const [moneyHover, viewMoneySublinks] = useState(false);
-
   return (
     <>
-      <div className={navbarstyles.topRow}>
+      <div className={navbarstyles.navContainer}>
         <div className={navbarstyles.linksContainer}>
-          {/*insert links */}
           <Link href='/'>
-            <a
-              className={
-                router.pathname == "/"
-                  ? navbarstyles.homeLink
-                  : navbarstyles.textLogo
-              }
-            >
-              astute
-            </a>
+            <a>astute</a>
           </Link>
+          <button
+            onClick={function myFunction() {
+              var x = document.getElementById("myLinks");
+              if (x.style.display === "block") {
+                x.style.display = "none";
+              } else {
+                x.style.display = "block";
+              }
+            }}
+          >
+            close
+          </button>
 
-          <div className={navbarstyles.linksRow}>
-            {/*Make links  */}
-            <span>
-              <Link href='/make'>
-                <a
-                  className={
-                    router.pathname == "/make"
-                      ? navbarstyles.makeLink
-                      : navbarstyles.menuMakeLink
-                  }
-                >
-                  <span>Make</span>
-                </a>
-              </Link>
-
-              <span>
-                <button onClick={() => viewMakeSublinks(!makeHover)}>+</button>
-              </span>
-            </span>
-
-            <Link href='/keep'>
-              <a
-                className={
-                  router.pathname == "/keep"
-                    ? navbarstyles.keepLink
-                    : navbarstyles.menuKeepLink
-                }
-              >
-                Keep
-              </a>
-            </Link>
-
-            <Link href='/grow'>
-              <a
-                className={
-                  router.pathname == "/grow"
-                    ? navbarstyles.growLink
-                    : navbarstyles.menuGrowLink
-                }
-              >
-                Grow
-              </a>
-            </Link>
-
-            <div>
-              <Link href='/money'>
-                <a
-                  className={
-                    router.pathname == "/money"
-                      ? navbarstyles.activeButton
-                      : navbarstyles.button
-                  }
-                >
-                  {" "}
-                  Your Money
-                </a>
-              </Link>
-            </div>
+          <div id='myLinks'>
+            <a>Test 2</a>
+            <a>Test 3</a>
           </div>
         </div>
       </div>
-
-      {/* this is the second row */}
-
-      {makeHover ? (
-        <div className={navbarstyles.secondRow}>
-          <div className={navbarstyles.linksContainer}>
-            <div className={navbarstyles.textLogoHidden}>astute</div>
-            <h1> test content</h1>
-          </div>
-        </div>
-      ) : null}
     </>
   );
 }
